3ad66c43430169eeea414210797c1b6509322063
trishal-singh <trishalsinghhcwheel@gmail.com>
2023-12-10 15:05:10 +0530

    added landing page for admin app and also did router setup

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { BrowserRouter ,Routes,Route} from 'react-router-dom'
+import Add from './components/Add.jsx'
+import Delete from './components/Delete.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+    <Routes>
+    <Route index element={<App />} />
+      <Route path="/add" element={<Add/>}/>
+      <Route path="/delete" element={<Delete/>}/>
+    </Routes>
+    
+    </BrowserRouter>
+  </React.StrictMode>
 )
