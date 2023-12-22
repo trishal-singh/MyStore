2cd17a70ec5fd6ae215794c08dd8c70e75266a73
trishal-singh <trishalsinghhcwheel@gmail.com>
2023-12-22 16:23:58 +0530

    setup mongodb and mongoose for backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose=require('mongoose')
+const connectDB=require('./dbConn')
+
+connectDB()
+mongoose.connection.once("open",()=>{
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello ')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
