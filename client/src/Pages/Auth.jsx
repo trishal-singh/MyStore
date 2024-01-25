331ce74b9ab58611d1dd7c367cd6f3762820be2e
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-01-25 22:25:46 +0530

    Added UI for Auth Page

--- client/src/Pages/Auth.jsx
+++ client/src/Pages/Auth.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Auth.css'
 const Auth = () => {
+    const [UserName,setUserName]= useState('')
+    const [Password,setPassword]=useState('')
+    const handleUserName = (e)=>{
+        setUserName(e.target.value)
+       }
+       const handlePassword = (e)=>{
+        setPassword(e.target.value)
+       }
   return (
     <>
-    <div className="logo">MyStore</div>
+    <div className="auth-logo">MyStore</div>
+    <div className='body'>
+    <form>
+    <label>
+       Username : <input type="text" value={UserName} onChange={handleUserName}></input>
+      </label>
+      <label>
+       Password : <input type="text" value={Password} onChange={handlePassword}></input>
+      </label>
+      <button type="submit">LOGIN</button>
+      <button type="submit">REGISTER</button>
+    </form>
+    </div>
     </>
   )
 }
