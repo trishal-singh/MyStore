246a0cb880601ee985bc0f3c49365238a519b300
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-01-27 22:37:04 +0530

    Added handler for login and register button

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -7,28 +7,33 @@ const {userData}=require('../Models/model')
 
 userRouter.post("/register", async (req, res) => {
     const { username, password } = req.body
+    
     try {
       const user = await userData.findOne({ username })
       if (user) {
         return res.status(400).json({ type:"USERNAME_ALREADY_EXISTS" })
       }
+      
       const hashedPassword = await bcrypt.hash(password, 10)
+      
       const newUser = new userData({ username, password: hashedPassword })
       await newUser.save()
+      
       res.json({ message: "User registered successfully" })
     } catch (err) {
-      res.status(500).json({ type: err })
+      console.log(err.message)
+      res.status(500).json({ type: err.message })
     }
   })
 
   userRouter.post("/login", async (req, res) => {
     const { username, password } = req.body
-  
+    
     try {
       const user = await userData.findOne({ username })
   
       if (!user) {
-        return res.status(400).json({ type: UserErrors.NO_USER_FOUND })
+        return res.status(400).json({ type: "NO_USER_FOUND" })
       }
       const isPasswordValid = await bcrypt.compare(password, user.password)
       if (!isPasswordValid) {
@@ -37,7 +42,7 @@ userRouter.post("/register", async (req, res) => {
       const token = jwt.sign({ id: user._id }, "secret")
       res.json({ token, userID: user._id })
     } catch (err) {
-      res.status(500).json({ type: err })
+      res.status(500).json({ type: err.message })
     }
   })
 
--- client/src/Pages/Auth.jsx
+++ client/src/Pages/Auth.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Auth.css'
+import axios from 'axios'
 const Auth = () => {
     const [UserName,setUserName]= useState('')
     const [Password,setPassword]=useState('')
@@ -9,19 +10,49 @@ const Auth = () => {
        const handlePassword = (e)=>{
         setPassword(e.target.value)
        }
+    const handleLogin = async (e)=>{
+       e.preventDefault()
+       try{
+        const result = await axios.post("http://localhost:3000/auth/login", {
+          "username":UserName,
+          "password":Password,
+        });
+        alert("Logged IN")
+        console.log(result)
+       }
+       catch(e)
+       {
+         alert(e)
+       }
+    }
+    const handleRegister = async (e)=>{
+      e.preventDefault()
+      try{
+       const result = await axios.post("http://localhost:3000/auth/register", {
+        "username":UserName,
+        "password":Password,
+       });
+       alert("Registered")
+       console.log(result)
+      }
+      catch(e)
+      {
+        alert(e)
+      }
+   }
   return (
     <>
     <div className="auth-logo">MyStore</div>
     <div className='body'>
     <form>
     <label>
-       Username : <input type="text" value={UserName} onChange={handleUserName}></input>
+       UserName : <input type="text" value={UserName} onChange={handleUserName}></input>
       </label>
       <label>
-       Password : <input type="text" value={Password} onChange={handlePassword}></input>
+       Password : <input type="password" value={Password} onChange={handlePassword}></input>
       </label>
-      <button type="submit">LOGIN</button>
-      <button type="submit">REGISTER</button>
+      <button onClick={handleLogin}>LOGIN</button>
+      <button onClick={handleRegister}>REGISTER</button>
     </form>
     </div>
     </>
