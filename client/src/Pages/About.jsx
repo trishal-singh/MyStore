58761612f2dcdcdf7cac73e5a54f741568aaa366
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-01-30 20:25:33 +0530

    Added About Page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import './About.css'
+import { useNavigate } from 'react-router'
 const About = () => {
+    const navigate=useNavigate()
   return (
     <>
     <Navbar/>
-    <div>About</div>
+    <main>
+    <p>Welcome to MyStore , your one stop shop for your fashion needs.</p>
+    <button className='mybutton' onClick={()=>navigate('/')}>SHOP NOW</button>
+    </main>
     </>
     
   )
