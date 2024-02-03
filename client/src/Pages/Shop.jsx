365ba18ae3553a1a407312dd2333e4f41b11b739
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-02-03 16:56:46 +0530

    Added products to the shop page

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -4,21 +4,22 @@ import { Navigate } from "react-router-dom";
 import Navbar from '../components/Navbar';
 import Container from '../components/Container';
 import { useGetProducts } from '../hooks/useGetProducts';
+import './Shop.css'
 const Shop = () => {
     const [cookies, _] = useCookies(["access_token"]);
     const { products } = useGetProducts();
-    console.log(products)
     if (!cookies.access_token) {
         return <Navigate to="/auth" />;
       }
   return (<>
           <Navbar/>
-          <div>Shop</div>
+          <div className='container'>
           {
             products.map((product)=>(
              <Container item={product}/>
             ))
           }
+          </div>
          </>
     
   )
